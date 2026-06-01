524024554321bef1bb1d40c930918c5c8a4d09e7
borayin30-svg <borayin30@gmail.com>
2026-06-01 14:36:59 +0700

    Add reports view

---
+++ app/views/reports/sales.php
@@ -0,0 +1,14 @@
+<?php include BASE_PATH . '/app/views/layouts/header.php'; ?>
+
+<div class="card">
+    <div class="card-header">
+        <h5 class="mb-0">Reports</h5>
+    </div>
+    <div class="card-body">
+        <div class="alert alert-info">
+            <i class="bi bi-graph-up"></i> Sales reports will be displayed here.
+        </div>
+    </div>
+</div>
+
+<?php include BASE_PATH . '/app/views/layouts/footer.php'; ?>
